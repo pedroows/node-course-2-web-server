9c6358792e33ed8c9f7e84e51ce27aecf15abf6b
PedroBrito <p.1392@hotmail.com>
2017-06-21 20:27:50 -0300

    New page projects

--- server.js
+++ server.js
@@ -23,12 +23,7 @@ app.use((req, res, next)=>{
     next();
 });
 
-app.use((req, res, next)=>{
-    res.render('maintance.hbs', {
-        pageTitle: 'Fixing things'
-    });
 
-});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -50,6 +45,13 @@ app.get('/', (req, res)=>{
 app.get('/about', (req, res)=>{
     res.render('about.hbs',{
       pageTitle: 'About Page'
+    });
+});
+
+app.get('/projects', (req, res)=>{
+    res.render('projects.hbs',{
+      pageTitle: 'About Page',
+      welcomeMessage: 'Portfoliio page here'
     });
 });
 
